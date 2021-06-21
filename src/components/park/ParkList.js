f3b48f6d709999f771d571feecefff3555fd2d77
Ashton Monzi <anmonzi@gmail.com>
2021-06-21 14:04:39 -0500

    added filter functionality to dog park page

--- src/components/park/ParkList.js
+++ src/components/park/ParkList.js
@@ -5,21 +5,40 @@ import "./Park.css"
 
 export const ParkList = () => {
     const { parks, getParks } = useContext(ParkContext)
+    const [ locationName, setLocationName ] = useState("all")
+    const [ filteredParks, setFilteredParks ] = useState([])
 
 
     useEffect(() => {
         getParks()
     }, [])
 
+    useEffect(() => {
+        locationName === "all" ? setFilteredParks(parks) : setFilteredParks(parks.filter(park => park.location.name === locationName))
+    }, [locationName])
+
+   
 
     return (
         <>
             <h1 className="parks-title title">Dog Parks in Davidson County</h1>
-
+            <h3 className="parksFilter-buttons">
+                <LocationButton name="all" handleSetLocationName={setLocationName} filterActive={ locationName === "all" ? true : false} /> /
+                <LocationButton name="Nashville" handleSetLocationName={setLocationName} filterActive={ locationName === "Nashville" ? true : false} /> /
+                <LocationButton name="East Nashville" handleSetLocationName={setLocationName} filterActive={ locationName === "East Nashville" ? true : false} /> /
+                <LocationButton name="Vaughns Gap" handleSetLocationName={setLocationName} filterActive={ locationName === "Vaughns Gap" ? true : false} /> /
+                <LocationButton name="Lincoya Hills" handleSetLocationName={setLocationName} filterActive={ locationName === "Lincoya Hills" ? true : false} /> /
+                <LocationButton name="Wedgewood - Houston" handleSetLocationName={setLocationName} filterActive={ locationName === "Wedgewood - Houston" ? true : false} /> /
+                <LocationButton name="Nashboro Village" handleSetLocationName={setLocationName} filterActive={ locationName === "Nashboro Village" ? true : false} /> /
+                <LocationButton name="Tusculum" handleSetLocationName={setLocationName} filterActive={ locationName === "Tusculum" ? true : false} /> /
+                <LocationButton name="Goodlettsville" handleSetLocationName={setLocationName} filterActive={ locationName === "Goodlettsville" ? true : false} /> /
+                <LocationButton name="Hermitage Woods" handleSetLocationName={setLocationName} filterActive={ locationName === "Hermitage Woods" ? true : false} /> /
+                <LocationButton name="Woodbine" handleSetLocationName={setLocationName} filterActive={ locationName === "Woodbine" ? true : false} />
+            </h3>
             <div className="parksParent__flex">
                 <div className="parks">
                     {
-                        parks.map(park => 
+                        filteredParks.map(park => 
                             <div className="park" key={park.id} id={`park--${park.id}`}>
                                 <div className="park__card">
                                     <h2 className="park__name">{ park.name }</h2>
@@ -34,4 +53,9 @@ export const ParkList = () => {
             </div>
         </>
     )
+}
+
+
+const LocationButton = ({ name, handleSetLocationName, filterActive }) => {
+    return <button className={ `filter-button ${ filterActive ? "active" : null}`} onClick={() => handleSetLocationName(name)}>{ name.toUpperCase() }</button>
 }
