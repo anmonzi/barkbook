72f4ece6e0d3f88266f1dad97ad00dde0b6906c4
Ashton Monzi <anmonzi@gmail.com>
2021-07-02 13:13:28 -0400

    added last minute style changes

--- src/components/location/LocationAnimalCards.js
+++ src/components/location/LocationAnimalCards.js
@@ -14,8 +14,8 @@ export const AnimalCard = ({ animalObj }) => {
                 <div><img src={animalObj.imageURL} className="animalCard__avatar" alt="Users dog"></img></div>
                 <div className="animal__info">
                     <div className="animal__name"><b>Name:</b> { animalObj.name } </div>
-                    <div className="animal__energy"><b>Energy Level:</b> { animalObj.animalEnergyLevel.energyLevel } </div>
                     <div className="animal__size"><b>Size:</b> {animalObj.animalSize.size}</div>
+                    <div className="animal__energy"><b>Energy Level:</b> { animalObj.animalEnergyLevel.energyLevel } </div>
                 </div>
             </div>
         </Link>
